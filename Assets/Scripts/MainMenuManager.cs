171ced154a549ef25286c4749a54e68362f68173
Gellért Botond László <laszlogb@edu.bme.hu>
2022-03-04 17:36:29 +0100

    Initial commit

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public enum MenuPage { MAIN, SETTINGS, CREDITS, CONFIRMEXIT }
+
+    public TextMeshProUGUI titleText;
+    public GameObject mainMenuHolder;
+    public GameObject confirmExitHolder;
+    public GameObject settingsHolder;
+
+    public string mainMenuTitle = "MAIN MENU";
+    public string settingsTitle = "SETTINGS";
+    string confirmExitTitle = "YOU SURE???";
+
+    MenuPage currentPage;
+
+    void Start()
+    {
+        currentPage = MenuPage.MAIN;
+        Application.targetFrameRate = 120;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            switch (currentPage)
+            {
+                case MenuPage.MAIN:
+                {
+                    ShowConfirmExit();
+                    break;
+                }
+                case MenuPage.SETTINGS:
+                {
+                    SettingsToMenu();
+                    break;
+                }
+                case MenuPage.CONFIRMEXIT:
+                {
+                    HideConfirmExit();
+                    break;
+                }
+            }
+        }
+    }
+
+    public void MenuToSettings()
+    {
+        titleText.text = settingsTitle;
+        currentPage = MenuPage.SETTINGS;
+        mainMenuHolder.SetActive(false);
+        settingsHolder.SetActive(true);
+    }
+
+    public void SettingsToMenu()
+    {
+        titleText.text = mainMenuTitle;
+        currentPage = MenuPage.MAIN;
+        settingsHolder.SetActive(false);
+        mainMenuHolder.SetActive(true);
+    }
+
+    public void ShowConfirmExit()
+    {
+        titleText.text = confirmExitTitle;
+        currentPage = MenuPage.CONFIRMEXIT;
+        confirmExitHolder.SetActive(true);
+    }
+
+    public void HideConfirmExit()
+    {
+        titleText.text = mainMenuTitle;
+        currentPage = MenuPage.MAIN;
+        confirmExitHolder.SetActive(false);
+    }
+
+    public void ConfirmExit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour
+{
+    enum SettingsPage { CONTROLS, AUDIO, DISPLAY, GRAPHICS}
+
+    public GameObject[] buttonBackgrounds;
+    public GameObject[] settingsPageHolders;
+
+    SettingsPage currentSettingsPage;
+
+    void Start()
+    {
+        currentSettingsPage = SettingsPage.CONTROLS;
+        SetCurrentButtonBackground();
+        SetCurrentSettingsPage();
+    }
+
+    public void ChangeSettingsPage(int pageIndex)
+    {
+        currentSettingsPage = (SettingsPage)pageIndex;
+        SetCurrentButtonBackground();
+        SetCurrentSettingsPage();
+    }
+
+    void SetCurrentButtonBackground()
+    {
+        for (int i = 0; i < buttonBackgrounds.Length; i++)
+        {
+            buttonBackgrounds[i].SetActive(i == (int)currentSettingsPage);
+        }
+    }
+
+    void SetCurrentSettingsPage() 
+    {
+        for (int i = 0; i < settingsPageHolders.Length; i++)
+        {
+            settingsPageHolders[i].SetActive(i == (int)currentSettingsPage);
+        }
+    }
+}
